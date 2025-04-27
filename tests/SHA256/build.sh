ee411563cbfa19246132f9698b0a7cf4050c608c
Ubuntu <ubuntu@ip-172-31-26-127.ec2.internal>
2025-04-27 22:17:33 +0000

    fix: migrate to Python3

--- tests/SHA256/build.sh
+++ tests/SHA256/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-./sha256gen.py -4
-( for i in $(seq 1 8); do echo $i; done ) | parallel python sha256gen.py -R -P -m
+python3 sha256gen.py -4
+( for i in $(seq 1 8); do echo $i; done ) | parallel python3 sha256gen.py -R -P -m
